342cf7b61b50ce7edd3bd68f88123ff7de01a65d
Rogerio Chebabi <rogerio@chebabi.net>
2026-02-09 23:08:08 +0000

    File changes

--- src/pages/DetalheCliente.jsx
+++ src/pages/DetalheCliente.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
 import { Link } from 'react-router-dom';
